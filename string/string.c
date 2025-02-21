d1fb1e9089da4aab19cebbd67ab8cb3330afa1e5
471D38UNNUX <aliwaskito123@gmail.com>
2025-02-22 01:41:33 +0700

    Update Repo

--- string/string.c
+++ string/string.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
-unsigned    long long hash(char *str)
+unsigned long long  hash(char *str)
 {
     unsigned long long  hasher = 14695981039346656037ULL, prime = 1099511628211;
 
@@ -10,59 +10,233 @@ unsigned    long long hash(char *str)
 
     return              hasher;
 }
-int         main()
+typedef struct      basic_string
 {
-    char                str1[] = "Hello";
-    char                str2[] = "World";
+    char    *data;
+    size_t  size;
+}                   basic_string;
+static basic_string *Cbasic_string(char *str, size_t size)
+{
+    basic_string    *new = (basic_string*)VirtualAlloc(NULL, sizeof(basic_string), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+    if              (!new) ExitProcess(1);
+
+    new->data       = (char*)VirtualAlloc(NULL, size + 1, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+    if              (!new->data) ExitProcess(1);
+    
+    strncpy_s(new->data, size + 1, str, size);
+
+    new->size       = size;
+
+    return          new;
+}
+static void         Dbasic_string(basic_string *basic_string[], size_t size)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        VirtualFree(basic_string[i]->data, 0, MEM_RELEASE);
+        VirtualFree(basic_string[i], 0, MEM_RELEASE);
+    }
+}
+static void         append_substr(basic_string *dest, char *str, size_t count)
+{
+    size_t                  new_size = dest->size + count;
+
+    char                    *new_data = (char *)VirtualAlloc(NULL, new_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    
+    if                      (!new_data) ExitProcess(1);
+
+    strncpy_s(new_data, new_size, dest->data, dest->size - 1);
+
+    strncat_s(new_data, new_size, str, count);
+
+    VirtualFree(dest->data, 0, MEM_RELEASE);
+
+    dest->data              = new_data;
+    dest->size              = new_size;
+}
+static void         append_basic_string(basic_string *dest, basic_string *str) {append_substr(dest, str->data, strnlen_s(str->data, SIZE_MAX));}
+static void         append_repeated_char(basic_string *dest, size_t count, char char_value)
+{
+    size_t                  new_size = dest->size + count;
+
+    char                    *new_data = (char *)VirtualAlloc(NULL, new_size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    
+    if                      (!new_data) ExitProcess(1);
+
+    strncpy_s(new_data, new_size, dest->data, dest->size - 1);
+    
+    memset(new_data + dest->size - 1, char_value, count);
+
+    new_data[new_size - 1]  = '\0';
+
+    VirtualFree(dest->data, 0, MEM_RELEASE);
+
+    dest->data              = new_data;
+    dest->size              = new_size;
+}
+static void         append_range(basic_string *dest, char *first, const char *last)
+{
+    if      (first >= last) return;
+
+    size_t  count = last - first;
+
+    append_substr(dest, first, count);
+}
+static void         assign_n(basic_string *dest, char *str, size_t count)
+{
+    size_t              new_size = strnlen_s(str, count);
+    char                *new_data = (char *)VirtualAlloc(NULL, new_size + 1, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    
+    if                  (!new_data) ExitProcess(1);
+
+    strncpy_s(new_data, new_size + 1, str, new_size);
+    
+    new_data[new_size]  = '\0';
+
+    if                  (dest->data) VirtualFree(dest->data, 0, MEM_RELEASE);
+    
+    dest->data = new_data;
+    dest->size = new_size;
+}
+static void         assign(basic_string *dest, char *str) {assign_n(dest, str, strnlen_s(str, _TRUNCATE));}
+static void         assign_substr(basic_string *dest, const basic_string *src, size_t off, size_t count)
+{
+    if (off >= src->size) return;
+    if (count > src->size - off) count = src->size - off;
+
+    assign_n(dest, src->data + off, count);
+}
+static void         assign_repeat(basic_string *dest, size_t count, char ch)
+{
+    char            *new_data = (char *)VirtualAlloc(NULL, count + 1, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    
+    if              (!new_data) ExitProcess(1);
+
+    memset(new_data, ch, count);
+    
+    new_data[count] = '\0';
+
+    if              (dest->data) VirtualFree(dest->data, 0, MEM_RELEASE);
+    
+    dest->data      = new_data;
+    dest->size      = count;
+}
+static void         assign_range(basic_string *dest, char *first, char *last)
+{
+    if (first >= last) return;
+
+    assign_n(dest, first, strnlen_s(first, last - first));
+}
+int                 main()
+{
+    basic_string        *str1 = Cbasic_string("Hello", 6);
+    basic_string        *str2 = Cbasic_string("World", 6);
     //  Concatenation
-    char                result[14];
+    basic_string        *result = Cbasic_string("", 14);
 
-    _snprintf_s(result, sizeof(result), _TRUNCATE, "%s, %s!", str1, str2);
+    _snprintf_s(result->data, result->size, _TRUNCATE, "%s, %s!", str1->data, str2->data);
 
-    fprintf_s(stdout, "%s\n", result);
+    fprintf_s(stdout, "%s\n", result->data);
     //  Getting string length
-    fprintf_s(stdout, "Length: %llu\n", strlen(result));
+    fprintf_s(stdout, "Length: %llu\n", result->size);
     //  Accessing characters
-    fprintf_s(stdout, "First character: %c\n", result[0]);
+    fprintf_s(stdout, "First character: %c\n", result->data[0]);
 
     //  Modifying a string
-    result[0]           = 'h';
+    result->data[0]     = 'h';
 
-    fprintf_s(stdout, "Modified string: %s\n", result);
+    fprintf_s(stdout, "Modified string: %s\n", result->data);
 
     //  Substring
-    char                sub[6];
+    basic_string        *sub = Cbasic_string("", 6);
 
-    strncpy_s(sub, sizeof(sub), result, 5);
+    strncpy_s(sub->data, sub->size, result->data, 5);
 
-    fprintf_s(stdout, "Substring: %s\n", sub);
+    fprintf_s(stdout, "Substring: %s\n", sub->data);
 
     //  Finding a substring
-    char                *pos = strstr(result, "World");
+    char                *pos = strstr(result->data, "World");
 
-    if                  (pos) fprintf_s(stdout, "'World' found at position: %llu\n", pos - result);
+    if                  (pos) fprintf_s(stdout, "'World' found at position: %llu\n", pos - result->data);
 
     //  Hash function (C++'s FNV-1a hash for demonstration)
-    unsigned long long  hashValue = hash(result);
+    unsigned long long  hashValue = hash(result->data);
 
     fprintf_s(stdout, "Hash value: %llu\n", hashValue);
     //  Swap function
-    fprintf_s(stdout, "Before swap: str1 = %s, str2 = %s\n", str1, str2);
+    fprintf_s(stdout, "Before swap: str1 = %s, str2 = %s\n", str1->data, str2->data);
 
     char                tmp[6];
     
-    strcpy_s(tmp, sizeof(tmp), str1);
-    strcpy_s(str1, sizeof(str1), str2);
-    strcpy_s(str2, sizeof(str2), tmp);
+    strncpy_s(tmp, sizeof(tmp), str1->data, str1->size);
+    strncpy_s(str1->data, str1->size, str2->data, str2->size);
+    strncpy_s(str2->data, str2->size, tmp, sizeof(tmp));
 
-    fprintf_s(stdout, "After swap: str1 = %s, str2 = %s\n", str1, str2);
+    fprintf_s(stdout, "After swap: str1 = %s, str2 = %s\n", str1->data, str2->data);
 
-    // Convert string to integer using strtol
-    char                numberStr[] = "12345";
+    //  Convert string to integer using strtol
+    basic_string        *numberStr = Cbasic_string("12345", 6);
     char                *endptr;
-    int                 number = strtol(numberStr, &endptr, 10);
+    int                 number = strtol(numberStr->data, &endptr, 10);
 
     fprintf_s(stdout, "Converted number: %d\n", number);
+
+    //  Convert number to string using _snprintf_s
+    basic_string        *numberToStr = Cbasic_string("", 14);
+
+    _snprintf_s(numberToStr->data, numberToStr->size, _TRUNCATE, "%d", number);
+
+    fprintf_s(stdout, "Converted string: %s\n", numberToStr->data);
+
+    //  String comparison using strcmp
+    if                  (strcmp(str1->data, str2->data)) fprintf_s(stdout, "Strings are not equal.\n");
+
+    char                fullLine[255];
+
+    //  Clear newline from previous input
+    getchar();
+
+    fprintf_s(stdout, "Enter a full line: ");
+
+    //  Using fgets function
+    if                  (fgets(fullLine, sizeof(fullLine), stdin))
+    {
+        size_t  len = strnlen_s(fullLine, sizeof(fullLine));
+
+        if      (len > 0 && fullLine[len - 1] == '\n') fullLine[len - 1] = '\0';
+
+        fprintf_s(stdout, "You entered: %s\n", fullLine);
+    }
+
+    //  Using basic_string struct
+    basic_string        *basicStr = Cbasic_string("Basic string example", 21);
+
+    fprintf_s(stdout, "Basic string: %s\n", basicStr->data);
+
+    //  Using append function
+    basic_string        *appendStr = Cbasic_string(" Appended text.", 16);
+
+    append_basic_string(result, appendStr);
+
+    fprintf_s(stdout, "After append: %s\n", result->data);
+
+    //  Using assign function
+    basic_string        *assignStr = Cbasic_string("", 1);
+
+    assign(assignStr, "Assigned text.");
+
+    fprintf_s(stdout, "After assign: %s\n", assignStr->data);
+
+    //  Using at function
+    char                thirdChar = result->data[2];
+
+    fprintf_s(stdout, "Character at index 2: %c\n", thirdChar);
+
+    basic_string        *All[] = {str1, str2, result, sub, numberStr, numberToStr, basicStr, appendStr, assignStr};
+
+    Dbasic_string(All, sizeof(All) / sizeof(All[0]));
 
     ExitProcess(0);
 }
--- string/string.cpp
+++ string/string.cpp
@@ -7,49 +7,92 @@ using namespace std;
 int main()
 {
     //  Declare and initialize strings
-    string          str1 = "Hello";
-    string          str2 = "World";
+    string              str1 = "Hello";
+    string              str2 = "World";
     //  Concatenation
-    string          result = str1 + ", " + str2 + "!";
+    string              result = str1 + ", " + str2 + "!";
 
-    cout            << result << endl;
+    cout                << result << endl;
     //  Getting string length
-    cout            << "Length: " << result.length() << endl;
+    cout                << "Length: " << result.length() << endl;
     //  Accessing characters
-    cout            << "First character: " << result[0] << endl;
+    cout                << "First character: " << result[0] << endl;
 
     //  Modifying a string
-    result[0]       = 'h';
+    result[0]           = 'h';
 
-    cout            << "Modified string: " << result << endl;
+    cout                << "Modified string: " << result << endl;
 
     //  Substring
-    string          sub = result.substr(0, 5);
+    string              sub = result.substr(0, 5);
 
     cout            << "Substring: " << sub << endl;
 
     //  Finding a substring
-    size_t          pos = result.find("World");
+    size_t              pos = result.find("World");
     
-    if              (pos != string::npos) cout << "'World' found at position: " << pos << endl;
+    if                  (pos != string::npos) cout << "'World' found at position: " << pos << endl;
 
     //  Hash function
-    hash<string>    hasher;
-    size_t          hashValue = hasher(result);
+    hash<string>        hasher;
+    size_t              hashValue = hasher(result);
 
-    cout            << "Hash value: " << hashValue << endl;
+    cout                << "Hash value: " << hashValue << endl;
     //  Swap function
-    cout            << "Before swap: str1 = " << str1 << ", str2 = " << str2 << endl;
+    cout                << "Before swap: str1 = " << str1 << ", str2 = " << str2 << endl;
 
     str1.swap(str2);
 
-    cout            << "After swap: str1 = " << str1 << ", str2 = " << str2 << endl;
+    cout                << "After swap: str1 = " << str1 << ", str2 = " << str2 << endl;
 
-    // Convert string to integer using stoi
-    string          numberStr = "12345";
-    int             number = stoi(numberStr);
+    //  Convert string to integer using stoi
+    string              numberStr = "12345";
+    int                 number = stoi(numberStr);
     
-    cout            << "Converted number: " << number << endl;
+    cout                << "Converted number: " << number << endl;
+
+    //  Convert number to string using to_string
+    string              numberToStr = to_string(number);
+
+    cout                << "Converted string: " << numberToStr << endl;
+
+    //  String comparison using != operator
+    if                  (str1 != str2) cout << "Strings are not equal." << endl;
+
+    //  Using getline function
+    cin.ignore();   //  Clear newline from previous input
+
+    string              fullLine;
+
+    cout                << "Enter a full line: ";
+
+    getline(cin, fullLine);
+
+    cout                << "You entered: " << fullLine << endl;
+
+    //  Using basic_string class
+    basic_string<char>  basicStr = "Basic string example";
+
+    cout                << "Basic string: " << basicStr << endl;
+
+    //  Using append function
+    string              appendStr = " Appended text.";
+
+    result.append(appendStr);
+
+    cout                << "After append: " << result << endl;
+
+    //  Using assign function
+    string              assignStr;
+
+    assignStr.assign("Assigned text.");
+
+    cout                << "After assign: " << assignStr << endl;
+
+    //  Using at function
+    char                thirdChar = result.at(2);
+
+    cout                << "Character at index 2: " << thirdChar << endl;
 
     ExitProcess(0);
 }
